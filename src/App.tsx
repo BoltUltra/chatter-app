7d7515c136f23f8772fdb6be35618987e1ec2660
BoltUltra <mayowa.u.sunusi@gmail.com>
2023-06-05 17:09:37 +0100

    Feat: Setup React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home.tsx";
+import Login from "./components/Login.tsx";
+import { loader } from "./assets";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    // simulate a delay of 2 seconds
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="bg-white flex items-center justify-center h-screen">
+        <img src={loader} className="w-44" alt="Loader" />
+      </div>
+    );
+  }
+
   return (
-    <>
-      <h3 className="font-bold text-4xl text-center text-blue-500">
-        Chatter App
-      </h3>
-    </>
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
