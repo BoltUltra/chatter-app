d9bdeaa6c2ca697eb596c55db1b2403801b07cc7
BoltUltra <mayowa.u.sunusi@gmail.com>
2023-06-05 16:23:07 +0100

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h3 className="font-bold text-4xl text-center text-blue-500">
+        Chatter App
+      </h3>
+    </>
+  );
+}
+
+export default App;
